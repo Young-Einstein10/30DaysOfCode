a94ad0f35151bbcfe8e6fc187b36ef5602fb8b59
young-einstein10 <aabdulrahmanyusuf@gmail.com>
2020-05-18 23:49:27 +0100

    Completed Day 18

---
+++ Day18/src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="">
+      <div className="container">
+          <p>Powered By <a href="https://newsapi.org/">News API</a> </p>
+          <p>
+            Latest News Across Nigeria
+          </p>
+        {/* <button>Theme Changer</button> */}
+      </div>
+      
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ Day18/src/pages/Dashboard.js
@@ -0,0 +1,102 @@
+import React, { useEffect, useState } from 'react';
+import Spinner from '../components/Spinner/Spinner';
+import uuid from 'react-uuid';
+
+
+const Dashboard = () => {
+  const [isLoading, setisLoading] = useState(true);
+  const [feeds, setFeeds] = useState([]);
+  const [searchInput, setsearchInput] = useState('');
+  const [feedError, setFeedError] = useState(null);
+
+  const url = 'https://newsapi.org/v2/top-headlines?country=ng&pageSize=90&apiKey=7e880dff532742e38183bea3a25100bb';
+
+  const searchUrl = `https://newsapi.org/v2/everything?q=${searchInput}&apiKey=7e880dff532742e38183bea3a25100bb`;
+
+
+  const handleFormSubmit = async e => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(searchUrl);
+      const data = await response.json()
+      console.log(data);
+      if (data.status === 'ok') {
+          setisLoading(false)
+          setFeeds(data.articles)
+        } else if (data.status === 'error') {
+          setFeedError(data.message)
+        }
+    } catch (error) {
+      console.log(error)
+      setFeedError(error.message)
+
+    }
+  }
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(url);
+        const data = await response.json()
+        console.log(data);
+        if (data.status === 'ok') {
+          setisLoading(false)
+          setFeeds(data.articles)
+        } else if (data.status === 'error') {
+          setFeedError(data.message)
+        }
+      } catch (error) {
+        console.log(error)
+        setFeedError(error.message)
+      }
+    }
+    fetchData()
+  }, []);
+
+    const FeedList = feeds.length ? (
+    feeds.map((feed) => {
+      const { source: { name }, title, description, url, urlToImage, publishedAt, } = feed;
+      return (
+        <article className="article" key={uuid()}>
+          <a href={url} className="image col-5" style={{backgroundImage: `url(${urlToImage})`}}></a>
+          <a href={url} className="article-details col-7">
+            <h2>{title}</h2>
+            <p>{description}</p>
+            <p>
+              <span>{name}</span>
+              <span>{publishedAt}</span>
+            </p>            
+          </a>
+        </article>
+      )
+    })
+  ) : (
+    <p>No Feeds available</p>
+  );
+
+  return (
+    <div className="container">
+      {/* <form action="#" onSubmit={handleFormSubmit}>
+        <label htmlFor="searchInput">
+          Enter a Topic to Search:
+          <input 
+            type="text"
+            className=""
+            value={searchInput}
+            name="searchInput"
+            onChange={e => setsearchInput(e.target.value)}
+            required
+          />
+        </label>
+        <button>Search</button>
+      </form> */}
+      <div className="row">
+        {feedError ? <p>{feedError}</p> : null}
+        {isLoading ? <Spinner /> : FeedList}
+      </div>
+    </div>
+  )
+}
+
+export default Dashboard;
