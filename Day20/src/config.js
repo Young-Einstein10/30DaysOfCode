0c076d073c38dab59edf66022f9f4e269e148b91
young-einstein10 <aabdulrahmanyusuf@gmail.com>
2020-05-23 21:56:29 +0100

    Fixed API Keys

--- Day20/src/config.js
+++
@@ -1,9 +0,0 @@
-export default {
-    zoomLevel: 16,
-    latitudeForFocusingMap: 51.4934,
-    longitudeForFocusingMap: 0.0098,
-    markerLongitude: 51.4934,
-    markerLatitude: 0.0098,
-    apiKey: 'AIzaSyClRPTwOpi-zC8cy4vL9YGtJdNG31c19Qc',
-    googleMapsMarkerIcon: 'marker icon link'
-};
